55f9aba07c88e0dc4d1fad39777952ed7411b388
jian9ang <jian9ang1@ya.ru>
2021-05-02 16:15:47 +0300

    исправил ошибки, которые мешали ходить по страницам и открывать корзину

---
+++ cart/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from.views import basket, basket_remove, basket_add
+
+urlpatterns = [
+    path('basket', basket, name='basket'),
+    path('basket_remove<product_id>', basket_remove, name='basket_remove'),
+    path('basket_add<product_id>', basket_add, name='basket_add'),
+
+]
